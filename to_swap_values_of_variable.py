a400cc898d44dbfe8bb2ca66f7477f943d06858c
Harshit_negi <142771674+demon2202@users.noreply.github.com>
2024-03-18 19:10:02 +0530

    To_swap_values_of_variable.py
    To swap without using a third variable

---
+++ to_swap_values_of_variable.py
@@ -0,0 +1,9 @@
+#To swap the value of two variable without using a third variable
+a=int(input("enter the first number :"))
+b=int(input("enter the second number :"))       # we can also use multiplication adn division to perporm the same task
+a=a+b   # a=a*b
+b=a-b   # b=a/b
+a=a-b   # a=a/b
+print("Swapping the values")
+print("Now the value of first number is :",a)
+print("now the value of second number is :",b)
