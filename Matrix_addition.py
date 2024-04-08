96ea510e1a7ad19af42186ffb77054ac95c855e7
Harshit Negi <142771674+demon2202@users.noreply.github.com>
2024-04-08 14:14:17 +0530

    Matrix_addition.py

---
+++ Matrix_addition.py
@@ -0,0 +1,8 @@
+x=[[1,2,3],[4,5,6,],[7,8,9]]
+y=[[10,11,12],[13,14,15],[16,17,18]]
+result=[[0,0,0],[0,0,0],[0,0,0]]
+for i in range(len(x)):
+    for j in range(len(x[0])):
+      result[i][j] =x[i][j]+y[i][j]
+for r in result:
+    print(r)
