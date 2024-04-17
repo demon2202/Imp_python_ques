459828b8cb4857a64a81da177bb23f247ca81b6f
Harshit Negi <142771674+demon2202@users.noreply.github.com>
2024-04-17 21:20:34 +0530

    Create emoji into text.py

---
+++ emoji into text.py
@@ -0,0 +1,12 @@
+#import demoji
+#demoji.download_codes()
+#for this project to work u need to install [demoji] in you pc/laptop
+#to install enter the above data in the terminal and let it download 
+
+#after that
+
+import demoji
+text ="""I will be our secret 👍 this year The party🥳 im hosting for you is
+going to be lit A happiness😍 of Have fun😊 till then you will erupt that day👌."""
+demoji.findall (text)
+
