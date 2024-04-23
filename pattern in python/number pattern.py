694d8533f3996c5c8a06289082a71d552d4960e7
Harshit Negi <142771674+demon2202@users.noreply.github.com>
2024-04-23 15:44:26 +0530

    Create number pattern.py

---
+++ pattern in python/number pattern.py
@@ -0,0 +1,17 @@
+"""
+1
+2  3
+4  5  6
+7  8  9  10
+11 12 13 14 15
+"""
+def num(n):
+    num = 1
+    for i in range(0, n):
+        for j in range(0, i+1):
+            print(num, end=" ")
+            num = num + 1
+        print("\r")
+
+n = 5
+num(n)
