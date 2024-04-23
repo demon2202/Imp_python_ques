f2c92693d0be71f6990df1203f8d502bbc7d5fe4
Harshit Negi <142771674+demon2202@users.noreply.github.com>
2024-04-23 15:42:01 +0530

    Create (Capital letters same for each row).py

---
+++ pattern in python/(Capital letters same for each row).py
@@ -0,0 +1,18 @@
+"""
+A
+BB
+CCC
+DDDD
+EEEEE
+"""
+def alphapat(n):
+    num = 65
+    for i in range(0, n):
+        for j in range(0, i+1):
+            ch = chr(num)
+            print(ch, end=" ")
+        num = num + 1
+    
+        print("\r")
+n = 5
+alphapat(n)
