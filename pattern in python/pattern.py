3f6ae2b9b7f7f67bcaab463bcabec20c16db714f
Harshit Negi <142771674+demon2202@users.noreply.github.com>
2024-04-23 15:39:01 +0530

    Create pattern.py

---
+++ pattern in python/pattern.py
@@ -0,0 +1,18 @@
+# Write a program to print the following .
+"""
+A
+BC
+DEF
+GHIJ
+KLMNO
+"""
+def num(n):
+    num = 1
+    for i in range(0, n):
+        for j in range(0, i+1):
+            print(num, end=" ")
+            num = num + 1
+        print("\r")
+
+n = 5
+num(n)
