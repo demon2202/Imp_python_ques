7384de1d7c4553e2ce92efc60c54b80fc46dd3a8
Harshit Singh Negi <142771674+demon2202@users.noreply.github.com>
2024-06-04 01:35:27 -0700

    Create using multiplication and division.py

---
+++ to_swap_values_of_variable/using multiplication and division.py
@@ -0,0 +1,9 @@
+#To swap the value of two variable without using a third variable
+a=int(input("enter the first number :"))
+b=int(input("enter the second number :"))       
+a=a*b
+b=a/b
+a=a/b
+print("Swapping the values")
+print("Now the value of first number is :",a)
+print("now the value of second number is :",b)
